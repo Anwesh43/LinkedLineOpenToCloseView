08134003223610d65c4b213f76d1a75ace5b32e1
Anwesh43 <anweshthecool0@gmail.com>
2019-02-16 11:48:36 +0530

    using renderer in view

--- lineopentocloseview/src/main/java/com/anwesh/uiprojects/lineopentocloseview/LineOpenToCloseView.kt
+++ lineopentocloseview/src/main/java/com/anwesh/uiprojects/lineopentocloseview/LineOpenToCloseView.kt
@@ -52,15 +52,16 @@ fun Canvas.drawLOCNode(i : Int, scale : Float, paint : Paint) {
 class LineOpenToCloseView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas)  {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
