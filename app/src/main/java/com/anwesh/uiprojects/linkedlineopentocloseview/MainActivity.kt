33549e4e4f7ca91033890fb8143ffe9701963882
Anwesh43 <anweshthecool0@gmail.com>
2019-02-16 12:12:56 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlineopentocloseview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlineopentocloseview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlineopentocloseview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.lineopentocloseview.LineOpenToCloseView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : LineOpenToCloseView = LineOpenToCloseView(this)
     }
 }
